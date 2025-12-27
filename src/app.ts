2548839cee36fb0ee01eb27cead34b0e8d190a84
akmal81 <sah64078@gmail.com>
2025-12-27 17:18:25 +0600

    Module-23 done

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application } from "express"
+import { postRouter } from "./modules/post/post.router";
+
+const app: Application = express();
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    res.send("Hello World")
+});
+
+app.use('/posts', postRouter)
+
+export default app
